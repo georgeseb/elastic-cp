47d16e289dab5387acca63c45f9633de41905d12
George Sebastian <gsebastian@yahoo-inc.com>
2016-08-09 22:02:25 -0700

    Color replicas / primaries differently. Move to next row. Still very raw code that need clean up.

--- app/controllers/cluster_controller.js
+++ app/controllers/cluster_controller.js
@@ -1,43 +1,62 @@
 app.controller("clusterController", ["$scope", "elasticsearchService", "transformationService", function($scope, elasticsearchService, transformationService){
 
-	elasticsearchService.getShardStore()
-						.then(function(resp) {
+	elasticsearchService
+	.getShardStore()
+	.then(function(resp) {
 
-									var trasformedData = transformationService.shardStoreToArray(resp.data)
+		var trasformedData = transformationService.shardStoreToArray(resp.data)
 
-									var svg = d3.select("#clusterGraphic")
-										.append("svg")
-											.attr("width", 1500)
-											.attr("height", 500);
-										
+		var svg = d3.select("#clusterGraphic")
+			.append("svg")
+				.attr("width", 1500)
+				.attr("height", 500);
+			
 
-									var arc = d3.arc()
-												.innerRadius(50)
-												.outerRadius(100)
-												.padAngle(.15)
+		var arc = d3.arc()
+					.innerRadius(50)
+					.outerRadius(100)
+					.padAngle(.15)
 
 
-									let i = 0;
-									for (key in trasformedData) {
+		let i = 0;
+		let downTranslate = 100;
 
-										let arcs = d3.pie()
-												 .value(d => 1)
-												 (trasformedData[key]);
+		for (key in trasformedData) {
 
-										svg
-											.append("g")
-											.selectAll("path")
-											.data(arcs)
-											.enter()
-												.append("path")
-												.attr("d", d => {return arc(d)})
-												.attr("transform", "translate(" + (100 + (i * 250)) + ", 100)")
-												.attr("stroke", "#130401")
-												.attr("stroke-width", 2)
-												.attr("fill", "#D89D91")
-										i++;
-									}
-						})
+			let arcs = d3.pie()
+					 .value(d => 1)
+					 (trasformedData[key]);
+
+			for (let i = 0; i < arcs.length; i++) {
+				arcs[i].data = trasformedData[key][i];
+			}
+
+			svg
+				.append("g")
+				.selectAll("path")
+				.data(arcs)
+				.enter()
+					.append("path")
+					.attr("d", d => {return arc(d)})
+					.attr("transform", "translate(" + (100 + (i * 250)) + ", " + downTranslate + ")")
+					.attr("stroke", "black")
+					.attr("stroke-width", .5)
+					.attr("fill", d => {
+						if (d.data.allocation == "primary") {
+							return "#8ECFE4";
+						}
+						return "#8EE498";
+					})
+
+			i++;
+			
+			if (i == 3) {
+				downTranslate += 250;
+				i = 0;
+			}
+			
+		}
+	})
 	
 	
 }]);
